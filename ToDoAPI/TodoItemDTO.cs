6a92729e360dbbf80952c8358cd1d1df42871023
Breno Rios <breno@brenorios.com.br>
2023-12-14 16:28:55 -0300

    Added posting excess protection

---
+++ ToDoAPI/TodoItemDTO.cs
@@ -0,0 +1,11 @@
+using ToDoAPI;
+public class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(Todo todoItem) =>
+    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+}
