b3cdaba3b2285d1cd840ebb481405e9b4c2743d9
Breno Rios <breno@brenorios.com.br>
2024-01-09 17:09:15 -0300

    Techmed initial commit

---
+++ techemd/techmed/Model.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Techmed.Model;
+public class TechmedContext : DbContext {
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Exam> Exams { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        base.OnConfiguring(optionsBuilder);
+        var connectionString = "server=localhost;user=dotnet;password=tic2023;database=techmed";
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Doctor>().ToTable("doctors").HasKey(d => d.Id);
+    }
+}
+
+public abstract class Person {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Doctor : Person {
+    public string Code { get; set; }
+    public string Specialization { get; set; }
+    public float Salary { get; set; }
+}
+
+public class Patient : Person {
+    public string Cpf { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+}
+
+public class Appointment {
+    public int number { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Patient patient{ get; set; }
+    public Doctor doctor{ get; set; }
+}
+
+public class Exam {
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+}
---
+++ techemd/techmed/Program.cs
@@ -0,0 +1,10 @@
+﻿using Techmed.Model;
+
+var db = new TechmedContext();
+
+// db.Doctors.Add(new Doctor(){
+//     Name = "Médico 1",
+//     Code = "COD001",
+//     Salary = 10000,
+//     Specialization = "Especialidade do Medico 1"
+// });
