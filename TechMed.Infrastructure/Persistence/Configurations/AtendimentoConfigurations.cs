991300af17bcf9a16a3aa485520c6acfeda5eaf4
Breno Rios <breno@brenorios.com.br>
2024-02-06 16:19:54 -0300

    Adicionadas relacoes

--- TechMed.Infrastructure/Persistence/Configurations/AtendimentoConfigurations.cs
+++ TechMed.Infrastructure/Persistence/Configurations/AtendimentoConfigurations.cs
@@ -8,5 +8,9 @@ public class AtendimentoConfigurations : IEntityTypeConfiguration<Atendimento>
     public void Configure(EntityTypeBuilder<Atendimento> builder)
     {
         builder.HasKey(a => a.AtendimentoId);
+
+        builder.HasOne(a => a.Paciente).WithMany(p => p.Atendimentos).HasForeignKey(a => a.PacienteId);
+        builder.HasOne(a => a.Medico).WithMany(m => m.Atendimentos).HasForeignKey(a => a.PacienteId);
+        builder.HasMany(a => a.Exames).WithOne(e => e.Atendimento).HasForeignKey(a => a.ExameId);
     }
 }
