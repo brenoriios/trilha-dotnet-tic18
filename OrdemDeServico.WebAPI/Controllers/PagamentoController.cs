dee1c35c86ff756ef1ef104eb31c8d5ec5a92305
Breno Rios <breno@brenorios.com.br>
2024-02-06 11:21:45 -0300

    Adicionado controller pagamento, adicionado pagamento input model

---
+++ OrdemDeServico.WebAPI/Controllers/PagamentoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdemDeServico.WebAPI.InputModels;
+
+namespace OrdemServico.WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class PagamentoController : ControllerBase
+{
+    [HttpGet("pagamentos")]
+    public IActionResult Get()
+    {
+        return NoContent();
+    }
+    [HttpGet("pagamento/{id}")]
+    public IActionResult GetById(int id)
+    {
+        return NoContent();
+    }
+    [HttpPost("pagamento")]
+    public IActionResult Post([FromBody] NewPagamentoInputModel pagamento)
+    {
+        return NoContent();
+    }
+    [HttpPut("pagamento/{id}")]
+    public IActionResult Put(int id, [FromBody] NewPagamentoInputModel pagamento)
+    {
+        return NoContent();
+    }
+    [HttpDelete("pagamento/{id}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+}
---
+++ OrdemDeServico.WebAPI/InputModels/NewPagamentoInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdemDeServico.WebAPI.InputModels;
+
+public class NewPagamentoInputModel
+{
+    public required float Valor { get; set; }
+    public required DateTimeOffset DataPagamento { get; set; }
+    public required string MetodoPagamento { get; set; }
+    public int OrdemServicoId { get; set; }
+}
